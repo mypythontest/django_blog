379b8dc8a9b51f80f104c02e8b82f342b8c975d5
create <createcheng@yeah.net>
2016-11-18 23:11:11 +0800

    替换多说为网易云跟帖。

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,3 @@
-var duoshuoQuery = {short_name:"createcheng"};
-	(function() {
-		var ds = document.createElement('script');
-		ds.type = 'text/javascript';ds.async = true;
-		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-		ds.charset = 'UTF-8';
-		(document.getElementsByTagName('head')[0]
-		 || document.getElementsByTagName('body')[0]).appendChild(ds);
-	})();
-
-
 $(function () {
     $.scrollUp({
         scrollName: 'scrollUp', // Element ID
@@ -21,3 +10,4 @@ $(function () {
         activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
 });
+
